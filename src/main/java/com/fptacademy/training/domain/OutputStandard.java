fa810ad0c7d1d9a08e0e539d6a8807574133db72
trichoip <107303342+trichoip@users.noreply.github.com>
2023-04-01 16:14:38 +0700

    fix some bug

--- src/main/java/com/fptacademy/training/domain/OutputStandard.java
+++ src/main/java/com/fptacademy/training/domain/OutputStandard.java
@@ -1,14 +1,13 @@
 package com.fptacademy.training.domain;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.io.Serializable;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -24,6 +23,7 @@ public class OutputStandard implements Serializable {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Schema(example = "1")
   private Long id;
 
   @Column(length = 10)
